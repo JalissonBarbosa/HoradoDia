43d239ac436868fc4b40df776ec08b536d211060
JalissonBarbosa <jalissonbcampos@gmail.com>
2020-11-25 17:46:03 -0300

    commit project

--- js/main.js
+++ js/main.js
@@ -3,19 +3,17 @@ function carregar() {
     var foto = document.getElementById("imagem");
 
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 20
+    var hora = data.getHours()
+    //var hora = 10  ----> Aqui você pode testar com horário predefinido!
     mensagem.innerHTML = `Agora são ${hora} horas.`
+
     if (hora >= 0 && hora < 12) {
-        //Bom dia!
         imagem.src = './img/manha.png'
         document.body.style.background = '#b89ea0';
     } else if (hora >= 12 && hora <= 18) {
-        //Boa tarde!
         imagem.src = './img/tarde.png'
         document.body.style.background = '#d76716';
     } else {
-        //Boa noite!
         imagem.src = './img/noite.png'
         document.body.style.background = '#4f3d57';
     }
